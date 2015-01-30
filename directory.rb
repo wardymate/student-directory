fbed5aeeead3001c1a6ad79615c13f9390b85711
Chris Ward <chriswarddev@gmail.com>
2015-01-30 17:56:38 +0000

    student array changed to a hash and the print method changed to work for the hash

--- directory.rb
+++ directory.rb
@@ -1,23 +1,24 @@
 # let's put all students into an array
 students = [
-"Dr. Hannibal Lecter",
-"darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex De large",
-"The Alien",
-"Terminator",
-"Freddy Kruger",
-"The Joker"]
+{:name => "Dr. Hannibal Lecter", :cohort => :november},
+{:name => "Darth Vader", :cohort => :november},
+{:name => "Nurse Ratched", :cohort => :november},
+{:name => "Michael Corleone", :cohort => :november},
+{:name => "Alex De large", :cohort => :november},
+{:name => "The Alien", :cohort => :november},
+{:name => "Terminator", :cohort => :november},
+{:name => "Freddy Kruger", :cohort => :november},
+{:name => "The Joker", :cohort => :november},
+]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "------------"
 end
 
-def print(names)
-	names.each do |name|
-	puts name
+def print(students)
+	students.each do |student|
+	puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
