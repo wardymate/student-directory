1ce2d8c24ce6f3b1ac89406d911b84adb72eef3c
Chris Ward <chriswarddev@gmail.com>
2015-01-30 18:13:39 +0000

    added user input to create the student directory

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-# let's put all students into an array
+# let's put all students into a hash
 students = [
 {:name => "Dr. Hannibal Lecter", :cohort => :november},
 {:name => "Darth Vader", :cohort => :november},
@@ -10,6 +10,8 @@ students = [
 {:name => "Freddy Kruger", :cohort => :november},
 {:name => "The Joker", :cohort => :november},
 ]
+
+#let's define our methods
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -26,7 +28,27 @@ def print_footer (names)
 	puts "Overall, we have #{names.length} great students"
 end
 
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create and empty array
+	students = []
+	#gets the first name
+	name = gets.chomp
+	#While the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {:name => name, :cohort => :november}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+	# return the array of students
+	students
+end
+
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
