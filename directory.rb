3e55f1fff588201a507fc347e83d9248ad89f3ed
Chris Ward <chriswarddev@gmail.com>
2015-01-30 16:58:25 +0000

    added iteration to the students array to remove repetitive code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# let's put all students into an array
 students = [
 "Dr. Hannibal Lecter",
 "darth Vader",
@@ -8,18 +9,13 @@ students = [
 "Terminator",
 "Freddy Kruger",
 "The Joker"]
-#and then print the,
+#and then print them
 puts "The students of my cohort at Makers Academy"
 puts "------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
+
+students.each do |student|
+	puts student
+end
 
 #finally, we print the total
 puts "Overall, we have #{students.length} great students"
