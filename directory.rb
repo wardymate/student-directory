2252bad6721493b78a1654f3738022da27de47b1
Chris Ward <chriswarddev@gmail.com>
2015-01-30 16:54:32 +0000

    students array created

--- directory.rb
+++ directory.rb
@@ -1,16 +1,26 @@
-student_count = 9
-# first, we print the list of students
+students = [
+"Dr. Hannibal Lecter",
+"darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex De large",
+"The Alien",
+"Terminator",
+"Freddy Kruger",
+"The Joker"]
+#and then print the,
 puts "The students of my cohort at Makers Academy"
 puts "------------"
-puts "Dr. Hannibal Lecter"
-puts "darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "alex De large"
-puts "The Alien"
-puts "Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+
 #finally, we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
 
