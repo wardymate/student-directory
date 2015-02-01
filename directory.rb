d4b75691ec34f724b42ce6439614765449c47c86
Chris Ward <chriswarddev@gmail.com>
2015-02-01 00:38:06 +0000

    Save to file menu item and function added

--- directory.rb
+++ directory.rb
@@ -56,6 +56,7 @@ end
 def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
 	puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -71,12 +72,26 @@ def process(selection)
 			@students = input_students
 		when "2"
 			show_students
+		when "3"
+			save_students
 		when "9"
 			exit
 		else
 			puts "I don't know what you meant, try again"
 	end
 end	
+
+def save_students
+	# open the file for writing
+	file = File.open("students.csv", "w")
+	# iterate over the array of students
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
+	end
+	file.close
+end
 
 
 interactive_menu
