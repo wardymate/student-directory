ecff0938bf36701d079e669b938e30fa30469cfc
Chris Ward <chriswarddev@gmail.com>
2015-02-01 01:16:28 +0000

    load file students function added

--- directory.rb
+++ directory.rb
@@ -57,6 +57,7 @@ def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "3. Save the list to students.csv"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -74,6 +75,8 @@ def process(selection)
 			show_students
 		when "3"
 			save_students
+		when "4"
+			load_students
 		when "9"
 			exit
 		else
@@ -93,6 +96,14 @@ def save_students
 	file.close
 end
 
+def load_students(filename = "students.csv")
+	file = File.open(filename, "r")
+	file.readlines.each do |line|
+		name, cohort = line.chomp.split(',')
+		@students << {:name => name, :cohort => cohort.to_sym}
+	end
+	file.close
+end
 
 interactive_menu
 
